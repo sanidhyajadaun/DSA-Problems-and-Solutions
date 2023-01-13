0e8e6e2d21bd72a45a018deb0ddbbee3b11080c7
Sanidhya Jadaun <77571526+sanidhyajadaun@users.noreply.github.com>
2023-01-14 01:35:35 +0530

    Add files via upload

---
+++ Arrays/12.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) 
+    {
+        vector<int> arr;
+        arr.push_back(first);
+        for(int i=0;i<encoded.size();i++)
+        {
+            arr.push_back(first^encoded[i]);
+            first=arr[i+1];
+        }
+        return arr;   
+    }
+};
