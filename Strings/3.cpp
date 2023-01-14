858803c7250272c7cd4f44ede574c8434ee4289f
Sanidhya Jadaun <77571526+sanidhyajadaun@users.noreply.github.com>
2023-01-15 00:48:17 +0530

    Add files via upload

---
+++ Strings/3.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) 
+    {
+        int i;
+        if(ruleKey=="type")
+        {
+            i=0;   
+        }
+        if(ruleKey=="color")
+        {
+            i=1;
+        }
+        if(ruleKey=="name")
+        {
+            i=2;
+        }
+        
+        int count=0;
+        for(int j=0;j<items.size();j++)
+        {
+            if(items[j][i]==ruleValue)
+            {
+                count++;
+            }
+        }
+        
+        return count;
+        
+    }
+};
