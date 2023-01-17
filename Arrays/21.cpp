de38c11526485b2955880b59c05244d9c2e1e36f
Sanidhya Jadaun <77571526+sanidhyajadaun@users.noreply.github.com>
2023-01-17 09:17:01 +0530

    Add files via upload

---
+++ Arrays/21.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProductDifference(vector<int>& nums) 
+    {
+        sort(nums.begin(),nums.end());
+
+        int ans=0,n;
+
+        n=nums.size();
+
+        ans=((nums[n-1]*nums[n-2])-(nums[0]*nums[1]));
+
+        return ans; 
+        
+    }
+};
