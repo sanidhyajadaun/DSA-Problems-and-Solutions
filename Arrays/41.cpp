00793f78d22cbabacc3f6ae62a9eea3fe6ba8c19
Sanidhya Jadaun <77571526+sanidhyajadaun@users.noreply.github.com>
2023-02-02 19:07:30 +0530

    Add files via upload

---
+++ Arrays/41.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) 
+    {
+        vector<vector<int>> ans;
+        int n = *max_element(groupSizes.begin(), groupSizes.end());
+        for(int i = 1; i <= n; i++) {
+            vector<int> subans;
+            for(int j = 0; j < groupSizes.size(); j++) {
+                if(groupSizes[j] == i) {
+                    if(subans.size() >= i) {
+                        ans.push_back(subans);
+                        subans.clear();
+                    }
+                    subans.push_back(j);
+                }
+            }
+            if(subans.size())
+            ans.push_back(subans);
+        }
+        return ans;
+    }
+};
