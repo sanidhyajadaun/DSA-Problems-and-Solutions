a93ea71c5ace388cec9114d8853b52d59bb47243
Sanidhya Jadaun <77571526+sanidhyajadaun@users.noreply.github.com>
2023-02-04 08:36:23 +0530

    Add files via upload

---
+++ Stack/1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) 
+    {
+        vector <int> ans(prices);
+        stack<int> st;
+        for(int i=0;i<prices.size();i++){
+            while(!st.empty() && prices[st.top()]>=prices[i]){
+                ans[st.top()]-=prices[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
