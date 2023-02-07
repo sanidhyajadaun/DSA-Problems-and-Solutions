b3a7019dee580c9c8fc60872a1f08855648c591f
Sanidhya Jadaun <77571526+sanidhyajadaun@users.noreply.github.com>
2023-02-07 21:34:03 +0530

    Add files via upload

---
+++ Arrays/46.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) 
+    {
+        vector<int> pos;
+        vector<int> neg;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]>0)
+            {
+                pos.push_back(nums[i]);
+            }
+            else
+            {
+                neg.push_back(nums[i]);
+            }
+        }
+        for(int i=0;i<nums.size()/2;i++)
+        {
+            ans.push_back(pos[i]);
+            ans.push_back(neg[i]);
+        }
+        return ans;
+    }
+};
