85737e531c4772bbdff8f4c9a708ba4c4ce764db
Sanidhya Jadaun <77571526+sanidhyajadaun@users.noreply.github.com>
2023-03-04 12:01:13 +0530

    Add files via upload

---
+++ Arrays/59.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) 
+    {
+        int count=0;
+        vector<int> expected;
+
+        for(auto i: heights)
+        {
+            expected.push_back(i);
+        }
+
+        sort(expected.begin(),expected.end());
+
+        for(int i=0;i<heights.size();i++)
+        {
+            if(heights[i]!=expected[i])
+            {
+                count++;
+            }
+        }
+
+        return count;
+        
+    }
+};
