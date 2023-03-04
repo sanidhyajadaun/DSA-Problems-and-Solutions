1a25f9f44b277603e3a7f7faf8128828a9186653
Sanidhya Jadaun <77571526+sanidhyajadaun@users.noreply.github.com>
2023-03-04 12:02:05 +0530

    Add files via upload

---
+++ Arrays/61.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) 
+    {
+        int left =0;
+        int right = nums.size()-1;
+        //Binary Search Algo
+        while(left<= right){
+            int mid = left + (right-left)/2;
+
+            // if target is found return its index
+            if(nums[mid] == target) return mid;
+
+            else if(nums[mid] > target) right = mid-1;
+            else left = mid+1;
+
+        }
+        //returning left index as it contains the position where it would be if it were inserted in order.
+        return left;
+    }
+};
