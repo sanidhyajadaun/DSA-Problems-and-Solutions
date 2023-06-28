e9830a37eb90f0733533cad9bac983412270af03
Sanidhya Jadaun <77571526+sanidhyajadaun@users.noreply.github.com>
2023-06-28 15:20:56 +0530

    Add files via upload

---
+++ Arrays/77.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hIndex(vector<int>& c) {
+        int ans=0;
+        sort(c.begin(),c.end());
+        for(int i=c.size()-1;i>=0;i--) {
+            if(c[i]>=c.size()-i) {
+                  ans++;
+            }  
+        }
+        return ans;
+    }
+};
