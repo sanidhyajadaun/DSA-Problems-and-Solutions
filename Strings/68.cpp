021ba3aee28711620886fef604cd1e40868efc21
Sanidhya Jadaun <77571526+sanidhyajadaun@users.noreply.github.com>
2023-07-08 11:58:37 +0530

    Is Subsequence: cpp

---
+++ Strings/68.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+        int i=0,j=0;
+        while(i<m && j<n)
+        {
+            if(s[i]==t[j])
+            {
+                i++;
+            }
+            j++;
+        }
+        if(i==m)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+};
